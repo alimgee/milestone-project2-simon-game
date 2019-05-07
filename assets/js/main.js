1fd04c2a30bd8e942bc81dd0afc90858c1f4e368
Alan mc Gee <mcgee.al@gmail.com>
2019-05-07 15:04:19 +0000

    Starting initial game level functionusing the set interval functionality. Calling game round function for 1 second. Gaame round function then runs until the stop interval function is called. Whilst the interval loops are less than the level variable the interval will keep running. I am calling a set timeout function which will light up a relevant pad during thistime and then increasing the lightpad tracker by 1. I am using console log to ensure that the loops are working correctly.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@ let playButton = $(".start-button");
 let playButtonText = $(".start-text");
 let playing = false;
 let computerSequence = [];
+let computerTurn=false;
+let interval = 0;
+let turn = 0;
+let lightPad = 0;
+let level = 0;
 
 
 // checking for clicks on 'play button'
@@ -16,15 +21,17 @@ playButton.click(function() {
 
 //function to set game in play
 function playGame() {
+    
+    level = 1; //level will increase with each repetition of the set interval loop
 
     // generate computer array sequence - loop 20 times as that is the max for the game
     for (var i = 0; i < 20; i++) {
         //placing random numbers between 1 -4 into computerSequence array
         computerSequence.push((getRndInteger(1, 4)));
     }
-    //testing computer sequence array by converting to string and sending to console
-    console.log(computerSequence.toString() +" length = " + computerSequence.length );
-
+    //begin computer turn by using set interval to loop throught the level
+    computerTurn =true;
+    interval=setInterval(gameRound, 1000);
 }
 
 //function to alternate to look of the play button during active play
@@ -40,4 +47,23 @@ function changeStatePlayButton() {
 //random function generator from w3c schools
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function gameRound(){
+    console.log("in game round");
+    if(lightPad == level ){//computer has completed a level once its lighted up same amoun of lights as level
+    //stop interval
+    clearInterval( interval);
+    console.log("stopping interval - level is " + level + " light pad indicator is " + lightPad);
+       
+    }else{
+        //light up a relevant light using timeout function
+        setTimeout(lightColour , 200);//switch light on for 200ms
+        lightPad ++; //tracking amount of lights switched on
+        
+    }
+}
+
+function lightColour(){
+    console.log("in light color")
 }
