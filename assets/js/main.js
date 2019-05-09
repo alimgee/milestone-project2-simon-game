811b074b7f10f766a578b65d4225dc81060ba619
Alan mc Gee <mcgee.al@gmail.com>
2019-05-09 18:57:30 +0000

    Adding first modal for the game rules link in the top nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,6 @@ function playGame() {
     lightPad = 0;
     win = false;
 
-
-    //level = 4; //level will increase with each repetition of the set interval loop
-
     // generate computer array sequence - loop 20 times as that is the max for the game
     for (var i = 0; i < 20; i++) {
         //placing random numbers between 1 -4 into computerSequence array
@@ -71,20 +68,17 @@ function getRndInteger(min, max) {
 }
 
 function gameRound() {
-    console.log("in game round Light pad is " + lightPad + "level is " + level);
+    
     showTextInDisplay("Round " + level);
     if (lightPad == level) { //computer has completed a level once its lighted up same amoun of lights as level
         //stop interval
         clearInterval(interval);
         defaultColour(computerSequence[lightPad - 1]); //sets last lighted colour back to default
         computerTurn = false;
-
-
     }
     if (computerTurn && playing == true) {
         //light up a relevant light using timeout function
         //clear last pad colour bg css back to default
-        console.log("in comp turn")
         setTimeout(lightColour, 200); //switch light on for 200ms
         defaultColour(computerSequence[lightPad - 1]); //clear last pad to its default background colour
         lightPad++;
@@ -113,7 +107,7 @@ function pad(padNumber, colour) { //function takes pad id number and relevant co
 
 function defaultColour(padNumber) { //function takes pad id number and uses pad() function to reset colour
 
-    console.log("default colour function called. pad number is " + padNumber)
+    
     if (padNumber == 1) pad(1, "darkred");
     if (padNumber == 2) pad(2, "darkgreen");
     if (padNumber == 3) pad(3, "darkblue");
@@ -125,7 +119,7 @@ function defaultColour(padNumber) { //function takes pad id number and uses pad(
 pad1.click(function() {
     //only doing something when its not the computers turn and the play button is pressed
     if (!computerTurn && playing) {
-        audio1.play();
+        audio1.play();// playing sound
         padUserClick(1, "pink"); // function to carry out actions after click
     }
 });
@@ -159,7 +153,7 @@ function padUserClick(num, colour) { // colour pad to 'ligth' after click
     setTimeout(function() {
         defaultColour(num); //after 300ms set bg colour back to default
     }, 300);
-    console.log(num)
+
     playerSequence.push(num); // passing user selections into array for later checking
     console.log(playerSequence.toString()); //testing
     checkSelection(); //checking user guesses
